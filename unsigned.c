1da04d4e20fe3797bf2f1fe3cb338dd6cbe10845
Anders A. Søndergaard <andersas@gmail.com>
2012-01-06 14:06:10 +0100

    Does your computer use signed or unsigned chars?

---
+++ unsigned.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+char a;
+unsigned char b;
+
+a = b = 128;
+a >>= 1;
+b >>= 1;
+printf( "\nYou'r computer has %ssigned char.\n\n", a == b ? "un" : "" ); 
+
+return 0;
+}
